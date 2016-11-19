0016c1771af3a4ed347cbe5ac7dec666b0b6d8af
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-11-19 20:59:12 +0000

    implementing the rest of the table class

--- orm.php
+++ orm.php
@@ -3,6 +3,7 @@
 	// Interfaces
 
 	interface iRow {
+		public function geta();
 		public function get_json();
 		public function get($column);
 		public function set($column, $value);
@@ -13,7 +14,7 @@
 		public function get_json();
 		public function get_columns();
 		public function get_table_name();
-		public function add_row()
+		public function add_row($row);
 	}
 
 	interface iQueryBuilder {
@@ -37,6 +38,10 @@
 			}
 		}
 
+		public function geta() {
+			return $this->$row;
+		}
+
 		public function get($column) {
 			return $this->$row[$column];
 		}
@@ -53,9 +58,38 @@
 	class Table implements iTable {
 
 		var $tablename;
+		var $rows;
 
 		function __construct($tablename) {
 			$this->$tablename = $tablename;
+			$this->$rows = [];
+		}
+
+		public function add_row($row) {
+			array_push($this->rows, $row);
+		}
+
+		public function get_table_name() {
+			return $this->$tablename;
+		}
+
+		public function get_rows() {
+			return $this->$rows;
+		}
+
+		public function get_columns() {
+			if (count($this->$rows) > 0) {
+				return array_keys($this->rows[0]);
+			}
+			return null;
+		}
+
+		public function get_json() {
+			$tmp = [];
+			foreach ($this->$rows as $row) {
+				array_push($tmp, $row->geta());
+			}
+			return json_encode($tmp);
 		}
 	}
 	
