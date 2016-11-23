8c94fc823fa8f51b92ff589292fbacceb23aace5
SouhaibAttaiki <sohaib.attaiki@gmail.com>
2016-11-23 01:00:20 +0000

    fixing some bugs

--- orm.php
+++ orm.php
@@ -32,27 +32,23 @@
 		private $row;
 
 		function __construct($columns = []) {
-			if ($columns) {
-				foreach ($columns as $column) {
-					$this->$row[$column] = null;
-				}
-			}
+			$this->row = array();
 		}
 
 		public function geta() {
-			return $this->$row;
+			return $this->row;
 		}
 
 		public function get($column) {
-			return $this->$row[$column];
+			return $this->row[$column];
 		}
 
 		public function set($column, $value) {
-			$this->$row[$column] = $value;
+			$this->row[$column] = $value;
 		}
 
 		public function get_json() {
-			return json_encode($this->$row);
+			return json_encode($this->row);
 		}
 	}
 
@@ -62,8 +58,8 @@
 		private $rows;
 
 		function __construct($tablename) {
-			$this->$tablename = $tablename;
-			$this->$rows = [];
+			$this->tablename = $tablename;
+			$this->rows = [];
 		}
 
 		public function add_row($row) {
@@ -71,15 +67,15 @@
 		}
 
 		public function get_table_name() {
-			return $this->$tablename;
+			return $this->tablename;
 		}
 
 		public function get_rows() {
-			return $this->$rows;
+			return $this->rows;
 		}
 
 		public function get_columns() {
-			if (count($this->$rows) > 0) {
+			if (count($this->rows) > 0) {
 				return array_keys($this->rows[0]);
 			}
 			return null;
@@ -87,7 +83,7 @@
 
 		public function get_json() {
 			$tmp = [];
-			foreach ($this->$rows as $row) {
+			foreach ($this->rows as $row) {
 				array_push($tmp, $row->geta());
 			}
 			return json_encode($tmp);
@@ -102,22 +98,21 @@
 		private $connection;
 
 		function __construct($host, $user, $pass, $database) {
-			$this->$host = $host;
-			$this->$user = $user;
-			$this->$pass = $pass;
-			$this->$database = $database;
+			$this->host = $host;
+			$this->user = $user;
+			$this->pass = $pass;
+			$this->database = $database;
 		}
 
 		public function connect() {
-			$this->$connection = mysql_connect($this->$host, $this->$user, $this->$pass);
-			if(! $this->$connection ) {
-				die('Could not connect: ' . mysql_error());
+			$this->connection = mysqli_connect($this->host, $this->user, $this->pass, $this->database);
+			if(mysqli_connect_error()) {
+				die('Could not connect: ' . mysqli_connect_errno() . '-'  . mysqli_connect_error());
 			}
-			mysql_select_db( $this->$database );
 		}
 
 		public function close() {
-			mysql_close($this->$connection);
+			mysqli_close($this->connection);
 		}
 
 		public function select($tablename, $columns, $column = null, $value = null) {
@@ -130,31 +125,36 @@
 				$where_clause = sprintf($where_clause, $column, $value);
 				$sql =  $sql . " " . $where_clause;
 			}
-
-			for ($i=0; $i < count($columns); $i++) { 
-				$columns[$i] = "`" . $columns[$i] . "`";
+			
+			if ($columns != "*") {
+				for ($i=0; $i < count($columns); $i++) { 
+					$columns[$i] = "`" . $columns[$i] . "`";
+				}
+				$tmp_str = join(", ", $columns);
+			} else {
+				$tmp_str = "*";
 			}
-
-			$tmp_str = join(", ", $columns);
 			$sql = sprintf($sql, $tmp_str, $tablename);
 
-			$retval = mysql_query( $sql, $this->$connection );
+			$retval = mysqli_query( $this->connection, $sql );
 
 			if(! $retval ) {
-				die('Could not get data: ' . mysql_error());
+				die('Could not get data: ' . mysqli_error());
 			}
 
 			$table = new Table($tablename);
 
-			while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+			while($row = mysqli_fetch_assoc($retval)) {
 				$row_class = new Row($columns);
+
 				foreach ($row as $key => $value) {
 					$row_class->set($key, $value);
 				}
+				
 				$table->add_row($row_class);
 			}
 
-			mysql_free_result($retval);
+			mysqli_free_result($retval);
 
 			$this->close();
 
@@ -178,10 +178,10 @@
 			$tmp_str = join(", ", array_values($row->geta()));
 			$sql = sprintf($sql, $tmp_str);
 
-			$retval = mysql_query( $sql, $this->$connection );
+			$retval = mysqli_query( $this->connection, $sql );
 
 			if(! $retval ) {
-				die('Could not insert data: ' . mysql_error());
+				die('Could not insert data: ' . mysqli_error());
 			}
 
 			$this->close();
@@ -193,10 +193,10 @@
 			$sql = "DELETE FROM %s WHERE `%s` = %s";
 			$sql = sprintf($sql, $tablename, $column, $value);
 
-			$retval = mysql_query( $sql, $this->$connection );
+			$retval = mysqli_query( $this->connection, $sql );
 
 			if(! $retval ) {
-				die('Could not delete data: ' . mysql_error());
+				die('Could not delete data: ' . mysqli_error());
 			}
 
 			$this->close();
@@ -216,10 +216,10 @@
 			$tmp_str = join(", ", $tmp_str);
 			$sql = sprintf($sql, $tmp_str, $column, $value);
 
-			$retval = mysql_query( $sql, $this->$connection );
+			$retval = mysqli_query( $this->connection, $sql );
 
 			if(! $retval ) {
-				die('Could not update data: ' . mysql_error());
+				die('Could not update data: ' . mysqli_error());
 			}
 
 			$this->close();
