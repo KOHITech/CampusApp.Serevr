dc495e87a653dce1a3e4607b96f611d3d52c78b5
Salah Eddine Lahniche <mxpn88@gmail.com>
2017-01-12 02:47:59 +0000

    enhancing dashboard

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,59 @@
         <p class="Info2"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>info4</p>
     </div>
   </div><!--R2-->
+
   
   <div style="clear: both; float: none; height: 0;"></div>
   
+  <div class="hr"></div>
+
+  <div class="col-md-6 col-lg-6 col-sm-6 col-xs-12 events">
+    <h2 class="title">Events</h2>
+    <div class="event">
+      <div class="image"><img src="img/blood.png" alt=""></div>
+      <h3 class="title">Don du sang</h3>
+      <p class="date">20 October 17</p>
+      <p class="club">Centr'Act</p>
+    </div>
+    <div class="event">
+      <div class="image"><img src="img/karaoke.png" alt=""></div>
+      <h3 class="title">Karaoke</h3>
+      <p class="date">24 October 17</p>
+      <p class="club">My Centrale</p>
+    </div>
+    <div class="event">
+      <div class="image"><img src="img/conference.png" alt=""></div>
+      <h3 class="title">Conference</h3>
+      <p class="date">26 October 17</p>
+      <p class="club">Centrale Casa</p>
+    </div>
+    <div class="event">
+      <div class="image"><img src="img/party.png" alt=""></div>
+      <h3 class="title">Party</h3>
+      <p class="date">01 December 17</p>
+      <p class="club">My Centrale</p>
+    </div>
+    <div class="event">
+      <div class="image"><img src="img/theatre.png" alt=""></div>
+      <h3 class="title">Theatre</h3>
+      <p class="date">12 December 17</p>
+      <p class="club">My Centrale</p>
+    </div>
+
+  </div>
+  <div class="col-md-6 col-lg-6 col-sm-6 col-xs-12 events">
+    <h2 class="title">Projets</h2>
+    <div class="event">
+      <div class="image"><img src="img/science.png" alt=""></div>
+      <h3 class="title">Projets scientifiques</h3>
+      <p class="date">13 projets</p>
+    </div>
+    <div class="event">
+      <div class="image"><img src="img/project.png" alt=""></div>
+      <h3 class="title">Projets innovation</h3>
+      <p class="date">8 projects</p>
+    </div>
+
+  </div>
+  <div style="clear: both; float: none; height: 0;"></div>
 </div><!--conteneur1-->
